d08aa330f2cd24c3b86e3231df86b68bd9050ac1
vihangajayasuriya446 <vxhaaaaa@gmail.com>
2025-07-26 23:38:12 +0530

    refactor: remove explicit entities and add Articles & Categories modules
    - Removed explicit entities array in TypeORM config (using autoLoadEntities)
    - Added ArticlesModule and CategoriesModule
    - Retained existing JWT and validation configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
+import { ArticlesModule } from './articles/articles.module';
+import { CategoriesModule } from './categories/categories.module';
 import { AppController } from './app.controller';
 
 @Module({
@@ -22,13 +23,14 @@ import { AppController } from './app.controller';
         username: configService.getOrThrow<string>('DB_USERNAME'),
         password: configService.getOrThrow<string>('DB_PASSWORD'),
         database: configService.getOrThrow<string>('DB_NAME'),
-        entities: [User],
+        autoLoadEntities: true, // Automatically load all entities
         synchronize: configService.get<string>('NODE_ENV') !== 'production',
-        autoLoadEntities: true
       }),
     }),
     AuthModule,
     UsersModule,
+    ArticlesModule,
+    CategoriesModule,
   ],
   controllers: [AppController],
   providers: [],
